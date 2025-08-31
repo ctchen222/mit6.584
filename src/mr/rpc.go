f7e2042df811a06f3b66798903ab983a2fb2148c
CHENG-TING CHEN <abfa762466@gmail.com>
2025-09-01 02:37:53 +0800

    feat: Initial Lab Software Setup

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,51 +6,24 @@ package mr
 // remember to capitalize all names.
 //
 
-import (
-	"os"
-	"strconv"
-)
+import "os"
+import "strconv"
 
-type TaskType int
+//
+// example to show how to declare the arguments
+// and reply for an RPC.
+//
 
-const (
-	MapTask TaskType = iota
-	ReduceTask
-	WaitingTask
-	JobFinished
-)
+type ExampleArgs struct {
+	X int
+}
 
-func (t TaskType) String() string {
-	switch t {
-	case MapTask:
-		return "MapTask"
-	case ReduceTask:
-		return "ReduceTask"
-	case WaitingTask:
-		return "WaitingTask"
-	case JobFinished:
-		return "JobFinished"
-	default:
-		return "Unknown"
-	}
+type ExampleReply struct {
+	Y int
 }
 
 // Add your RPC definitions here.
-type WorkerArgs struct {
-	MapTaskNumber    int
-	ReduceTaskNumber int
-}
 
-type WorkerReply struct {
-	Tasktype int // 0: map task, 1: reduce task, 2: waiting, 3: job finished
-	NMap     int // number of map task
-	NReduce  int // number of reduce task
-
-	MapTaskNumber int    // map task only
-	Filename      string // maptask only
-
-	ReduceTaskNumber int // reducetask only
-}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,169 +1,77 @@
 package mr
 
-import (
-	"encoding/json"
-	"fmt"
-	"hash/fnv"
-	"io"
-	"io/ioutil"
-	"log"
-	"net/rpc"
-	"os"
-	"sort"
-	"strconv"
-	"time"
-)
+import "fmt"
+import "log"
+import "net/rpc"
+import "hash/fnv"
 
+
+//
 // Map functions return a slice of KeyValue.
+//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-// for sorting by key.
-type ByKey []KeyValue
-
-// for sorting by key.
-func (a ByKey) Len() int           { return len(a) }
-func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
-
+//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
+//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+
+//
 // main/mrworker.go calls this function.
+//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
+
 	// Your worker implementation here.
-	for {
-		args := WorkerArgs{}
-		reply := WorkerReply{}
-		ok := call("Coordinator.AllocateTask", &args, &reply)
-		if !ok || reply.Tasktype == 3 {
-			break
-		}
-		if reply.Tasktype == 0 {
-			intermediate := []KeyValue{}
 
-			file, err := os.Open(reply.Filename)
-			if err != nil {
-				log.Fatalf("cannot open %v", reply.Filename)
-			}
-			content, err := io.ReadAll(file)
-			if err != nil {
-				log.Fatalf("cannot read %v", reply.Filename)
-			}
-			file.Close()
+	// uncomment to send the Example RPC to the coordinator.
+	// CallExample()
 
-			kva := mapf(reply.Filename, string(content))
-			// fmt.Printf("kva %v\n", kva)
-			intermediate = append(intermediate, kva...)
-			// fmt.Printf("intermediate %v\n", intermediate)
-
-			buckets := make([][]KeyValue, reply.NReduce)
-			for i := range buckets {
-				buckets[i] = []KeyValue{}
-			}
-			for _, kva := range intermediate {
-				buckets[ihash(kva.Key)%reply.NReduce] = append(buckets[ihash(kva.Key)%reply.NReduce], kva)
-			}
-
-			// write into intermediate files
-			for i := range buckets {
-				oname := "mr-" + strconv.Itoa(reply.MapTaskNumber) + "-" + strconv.Itoa(i)
-				ofile, _ := ioutil.TempFile("", oname+"*")
-				enc := json.NewEncoder(ofile)
-				for _, kva := range buckets[i] {
-					err := enc.Encode(&kva)
-					if err != nil {
-						log.Fatalf("cannot write into %v", oname)
-					}
-				}
-				os.Rename(ofile.Name(), oname)
-				ofile.Close()
-			}
-			// call master to send the finish message
-			finishedArgs := WorkerArgs{reply.MapTaskNumber, -1}
-			finishedReply := WorkerReply{}
-			call("Coordinator.ReceiveFinishedMap", &finishedArgs, &finishedReply)
-		} else if reply.Tasktype == 1 {
-			// reduce task
-			// collect key-value from mr-X-Y
-			intermediate := []KeyValue{}
-			for i := 0; i < reply.NMap; i++ {
-				iname := "mr-" + strconv.Itoa(i) + "-" + strconv.Itoa(reply.ReduceTaskNumber)
-				// open && read the file
-				file, err := os.Open(iname)
-				if err != nil {
-					log.Fatalf("cannot open %v", file)
-				}
-				dec := json.NewDecoder(file)
-				for {
-					var kv KeyValue
-					if err := dec.Decode(&kv); err != nil {
-						break
-					}
-					intermediate = append(intermediate, kv)
-				}
-				file.Close()
-			}
-			// sort by key
-			sort.Sort(ByKey(intermediate))
-
-			// output file
-			oname := "mr-out-" + strconv.Itoa(reply.ReduceTaskNumber)
-			ofile, _ := ioutil.TempFile("", oname+"*")
-
-			//
-			// call Reduce on each distinct key in intermediate[],
-			// and print the result to mr-out-0.
-			//
-			i := 0
-			for i < len(intermediate) {
-				j := i + 1
-				for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
-					j++
-				}
-				values := []string{}
-				for k := i; k < j; k++ {
-					values = append(values, intermediate[k].Value)
-				}
-				output := reducef(intermediate[i].Key, values)
-
-				// this is the correct format for each line of Reduce output.
-				fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
-
-				i = j
-			}
-			os.Rename(ofile.Name(), oname)
-			ofile.Close()
-
-			for i := 0; i < reply.NMap; i++ {
-				iname := "mr-" + strconv.Itoa(i) + "-" + strconv.Itoa(reply.ReduceTaskNumber)
-				err := os.Remove(iname)
-				if err != nil {
-					log.Fatalf("cannot open delete" + iname)
-				}
-			}
-
-			// send the finish message to master
-			finishedArgs := WorkerArgs{-1, reply.ReduceTaskNumber}
-			finishedReply := WorkerReply{}
-			call("Coordinator.ReceiveFinishedReduce", &finishedArgs, &finishedReply)
-		}
-		time.Sleep(time.Second)
-	}
-	return
 }
 
+//
+// example function to show how to make an RPC call to the coordinator.
+//
+// the RPC argument and reply types are defined in rpc.go.
+//
+func CallExample() {
+
+	// declare an argument structure.
+	args := ExampleArgs{}
+
+	// fill in the argument(s).
+	args.X = 99
+
+	// declare a reply structure.
+	reply := ExampleReply{}
+
+	// send the RPC request, wait for the reply.
+	// the "Coordinator.Example" tells the
+	// receiving server that we'd like to call
+	// the Example() method of struct Coordinator.
+	ok := call("Coordinator.Example", &args, &reply)
+	if ok {
+		// reply.Y should be 100.
+		fmt.Printf("reply.Y %v\n", reply.Y)
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+}
+
+//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
+//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
